17668d03fdf4a8bdf8846e0a9619afed6120aee2
adam-mlc <adam.laviguer@mlchq.com>
2025-04-28 13:47:49 -0700

    Update exportLblClientScript.js

--- exportLblClientScript.js
+++ exportLblClientScript.js
@@ -12,7 +12,7 @@ define([], function() {
         var itemId = nlapiGetRecordId();
         var quantity = prompt('Please enter the quantity:');
         if (quantity !== null && quantity !== '') {
-            var suiteletUrl = 'INSERT URL&itemId=' + itemId + '&quantity=' + quantity;
+            var suiteletUrl = 'INSERT URL&itemId=' + itemId + '&quantity=' + quantity; // Replace "INSERT URL" with the URL of the Suitelet
             window.open(suiteletUrl, '_blank');
         } else {
             alert('Quantity is required to print the LBL.');
